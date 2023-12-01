4006a13c44286e7f757f2f19f786dbdca14d261a
Shahmeer-Sheraz <103183988+Shahmeer-Sheraz@users.noreply.github.com>
2023-12-01 21:33:17 +0500

    javadoc comments
    added

--- ps3/src/expressivo/Addition.java
+++ ps3/src/expressivo/Addition.java
@@ -3,20 +3,45 @@ package expressivo;
 
 import java.util.Objects;
 
+/**
+ * An immutable class representing an addition expression in a polynomial.
+ * Addition expressions consist of a left and a right operand.
+ *
+ * <p>PS3 instructions: this class is a concrete variant of the Expression interface.
+ * It represents an addition operation in a polynomial expression.
+ */
 public class Addition implements Expression {
     private final Expression left;
     private final Expression right;
 
+    /**
+     * Creates a new Addition expression with the given left and right operands.
+     *
+     * @param left  the left operand, must not be null
+     * @param right the right operand, must not be null
+     * @throws NullPointerException if left or right is null
+     */
     public Addition(Expression left, Expression right) {
         this.left = Objects.requireNonNull(left, "Left operand cannot be null");
         this.right = Objects.requireNonNull(right, "Right operand cannot be null");
     }
 
+    /**
+     * Returns a string representation of the addition expression.
+     *
+     * @return a string representing the addition operation with operands
+     */
     @Override
     public String toString() {
         return "(" + left.toString() + " + " + right.toString() + ")";
     }
 
+    /**
+     * Checks if this addition expression is equal to another object.
+     *
+     * @param thatObject the object to compare with
+     * @return true if the objects are equal, false otherwise
+     */
     @Override
     public boolean equals(Object thatObject) {
         if (this == thatObject) return true;
@@ -25,6 +50,11 @@ public class Addition implements Expression {
         return left.equals(addition.left) && right.equals(addition.right);
     }
 
+    /**
+     * Computes the hash code of the addition expression.
+     *
+     * @return the hash code
+     */
     @Override
     public int hashCode() {
         // Custom hash code calculation based on the hash codes of the subexpressions
@@ -32,6 +62,11 @@ public class Addition implements Expression {
         int rightHash = (right == null) ? 0 : right.hashCode();
         return 31 * leftHash + rightHash;
     }
+
+    // Abstraction Function: Represents an addition expression with left and right operands.
+    // Rep Invariant: Both left and right operands must not be null.
+
+    // No checkRep is needed here as the class maintains a simple and clear rep invariant.
 
     // Additional instance methods for Addition can be added here
 }
--- ps3/src/expressivo/Variable.java
+++ ps3/src/expressivo/Variable.java
@@ -3,18 +3,42 @@ package expressivo;
 
 import java.util.Objects;
 
+/**
+ * An immutable class representing a variable expression in a polynomial.
+ * Variables are case-sensitive nonempty strings of letters.
+ *
+ * <p>PS3 instructions: this class is a concrete variant of the Expression interface.
+ * It represents a variable in a polynomial expression.
+ */
 public class Variable implements Expression {
     private final String name;
 
+    /**
+     * Creates a new Variable with the given name.
+     *
+     * @param name the name of the variable, must be a nonempty string of letters
+     * @throws NullPointerException if the name is null
+     */
     public Variable(String name) {
         this.name = Objects.requireNonNull(name, "Variable name cannot be null");
     }
 
+    /**
+     * Returns a string representation of the variable.
+     *
+     * @return the name of the variable
+     */
     @Override
     public String toString() {
         return name;
     }
 
+    /**
+     * Checks if this variable is equal to another object.
+     *
+     * @param thatObject the object to compare with
+     * @return true if the objects are equal, false otherwise
+     */
     @Override
     public boolean equals(Object thatObject) {
         if (this == thatObject) return true;
@@ -26,6 +50,11 @@ public class Variable implements Expression {
         return name.equals(variable.name);
     }
 
+    /**
+     * Computes the hash code of the variable.
+     *
+     * @return the hash code
+     */
     @Override
     public int hashCode() {
         // Custom hash code calculation based on the characters of the name
@@ -34,8 +63,13 @@ public class Variable implements Expression {
             hash = 31 * hash + c;
         }
         return hash;
+    }
 
+    // Abstraction Function: Represents a variable expression with a unique name.
+    // Rep Invariant: The name must be a nonempty string of letters.
+
+    // No checkRep is needed here as the class maintains a simple and clear rep invariant.
 
     // Additional instance methods for Variable can be added here
 }
-}
+
